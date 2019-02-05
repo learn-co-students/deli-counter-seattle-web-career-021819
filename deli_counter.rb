ed74ff602e7899899890d47b4f0bd3fea91b9fe3
clairemuller <clairermuller@gmail.com>
2019-02-04 21:37:40 -0800

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+
+def line(katz_deli)
+
+  if katz_deli.empty?
+    puts "The line is currently empty."
+
+  else
+    string = "The line is currently:"
+    katz_deli.each_with_index do |name, index|
+      string << " #{index + 1}. #{name}"
+
+  end
+end
+
+def take_a_number(katz_deli, new_person)
+
+  if katz_deli.empty?
+    katz_deli << new_person
+    puts "Welcome, #{new_person}. You are number 1 in line."
+
+  else
+    katz_deli << new_person
+    puts "Welcome, #{new_person}. You are number #{katz_deli.length} in line."
+
+  end
+end
